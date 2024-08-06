771b7382c21727f88db5133607143aa3b244c63b
wepplerj <jnweppler@gmail.com>
2024-08-06 08:53:02 -0400

    pdf issues

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,13 +3,13 @@ import './index.css';
 const Resume = () => {
     return (
         <div className="resume-container">
-            <h2>Testing</h2>
+            <h2>Resume</h2>
             <p>View and download my resume:</p>
             <div style={{ width: '100%', height: '800px', overflow: 'auto' }}>
-                <embed src={`${process.env.PUBLIC_URL}/public/Weppler-Resume-2025.pdf`} type="application/pdf" width="100%" height="100%" />
+                <embed src='my-portfolio/docs/assets/Weppler-Resume-2025.pdf' type="application/pdf" width="100%" height="100%" />
             </div>
             <p>
-                <a href="public/Weppler-Resume-2025.pdf" download="Weppler-Resume-2025.pdf">
+                <a href="my-portfolio/docs/assets/Weppler-Resume-2025.pdf" download="Weppler-Resume-2025.pdf">
                     Download Resume (PDF)
                 </a>
             </p>
